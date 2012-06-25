559d5aa7d0dbdadef78d1e83a54f2cad120a508d
Jeppe Pihl <jpihl08@gmail.com>
2012-06-25 14:24:13 +0200

    added plotato client

---
+++ example.py
@@ -0,0 +1,59 @@
+from plotatoclient import PlotatoClient
+
+#settings:
+rest_url    = "http://127.0.0.1:8000/api/v1/"
+project_key = ""
+test_key    = ""
+password    = ""
+
+def main():
+    p = PlotatoClient(rest_url)
+    
+    
+    print "adding test"
+    p.add_test(password, project_key, "Test Test", "This is a test test, created using the rest api.")
+    
+    print "adding run"
+    p.add_run(password, test_key, ["Hello World!", 42, {"color" : "Bright Yellow", "age" : 32}])
+    
+    print "getting projects"
+    projects = p.get_projects()
+    print "total count is {0} projects".format(projects["meta"]["total_count"])
+    print "done"
+
+    print "getting project"
+    project = p.get_projects("key="+project_key)
+    print project["objects"]
+    print "done"
+
+    print "gettings tests"
+    tests = p.get_tests()
+
+    print "total count is {0} tests".format(tests["meta"]["total_count"])
+    print "done"
+
+    print "gettings tests from project"
+    tests = p.get_tests("project="+project_key)
+    print "total count is {0} tests".format(tests["meta"]["total_count"])
+    print "done"
+
+    print "gettings atmost 2 tests from project"
+    tests = p.get_tests("project="+project_key+"&limit=2")
+    print "recieved {0} tests".format(len(tests["objects"]))
+    print "done"
+
+    print "getting runs"
+    runs = p.get_runs()
+    print "total count is {0} runs".format(runs["meta"]["total_count"])
+    print "done"
+
+    print "getting runs from test"
+    runs = p.get_runs("test="+test_key)
+    print "total count is {0} runs".format(runs["meta"]["total_count"])
+    for run in runs["objects"]:
+        print run["data"][2]["color"]
+    print "done"
+
+
+if __name__ == '__main__':
+    main()
